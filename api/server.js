e058087053b0264c258fbb57443d849377f9d705
William Moon <william-moon@lambdastudents.com>
2021-07-09 11:51:41 -0700

    server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+server.use('/', (req, res) => {
+    res.status(404).json({message: "check go bouncy bounce"})
+});
 
 module.exports = server;
